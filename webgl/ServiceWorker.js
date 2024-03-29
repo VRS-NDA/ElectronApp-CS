8aac5fa28c36dd3131ee806f8b5cb62f97905fa0
Umar Farooq <mufaawan@gmail.com>
2024-03-29 05:59:45 +0500

    fixing-cube-detection-[temp]

--- webgl/ServiceWorker.js
+++ webgl/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "VirtualFTC-Robot Simulator-0.1";
 const contentToCache = [
-    "Build/Build.loader.js",
-    "Build/Build.framework.js.unityweb",
-    "Build/Build.data.unityweb",
-    "Build/Build.wasm.unityweb",
+    "Build/build.loader.js",
+    "Build/build.framework.js.unityweb",
+    "Build/build.data.unityweb",
+    "Build/build.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
